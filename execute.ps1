06a61829d82aa03a83137ba47ccdee5e1ecb6a6b
Norman Yard <67605918+noyard@users.noreply.github.com>
2025-09-18 13:00:49 -0500

    Add installation instructions to execute.ps1
    Added comments for installation instructions and ensured proper formatting.

--- execute.ps1
+++ execute.ps1
@@ -1,2 +1,6 @@
 ﻿
+
 .\DownloadAllVS2022Workloads.ps1 -bootstrapper "VS2022Ent.exe" -DestinationPath ".\VS2022Enterprise" -Language "en-US"
+
+##to install
+#".\VS2022Enterprisevs_enterprise.exe" --noWeb --passive
